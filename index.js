0ed3b4296e1952242d029094449002db56547e50
Golemite02 <97048204+Golemite02@users.noreply.github.com>
2022-01-03 18:32:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const discord = require('discord.js');
+require('discord-reply');
+const { Client } = require('discord.js')
+const { GiveawaysManager } = require('discord-giveaways');
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        embedColor: "FF0000",
+        reaction: "🎉"
+    }
+});
+
+
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
+
+["aliases", "commands"].forEach(x => client[x] = new Collection());
+["command", "event"].forEach(x => require(`./handlers/${x}`)(client));
+
+require('./server')();
+
+client.login(process.env.TOKEN);
